0b1d9ecfb7b06f658be0955fc54fb99677be2edb
Matt Lewis <domattthings@gmail.com>
2019-01-27 13:46:42 -0800

    Remove word

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Photos provided by <a href="https://www.pexels.com/">Pexels</a></p>
         <p>Made by <a href="https://mattmatters.io">Matt Lewis</a></p>
-        <p>Stuff <a href="https://gitlab.com/mattmatters/jpegdog"><font-awesome-icon icon="gitlab"></font-awesome-icon></a></p>
+        <p><a href="https://gitlab.com/mattmatters/jpegdog"><font-awesome-icon icon="gitlab"></font-awesome-icon></a></p>
     </div>
 </template>
 

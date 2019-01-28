958ce6642e9b6907699e5b52386a17ec178fde8b
Matt Lewis <domattthings@gmail.com>
2019-01-27 16:25:30 -0800

    Make a test

---
+++ frontend/tests/unit/footer.spec.ts
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils';
+import Footer from '@/components/Footer.vue';
+
+describe('Footer.vue', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = shallowMount(Footer, {});
+    expect(wrapper.text()).toMatch("Matt");
+  });
+});
